3b60e9b5109dc334aa7476014bf2fa0628400d87
Júlio César Lima Reis <juliolimareis@gmail.com>
2023-05-08 08:21:54 -0300

    Fix: Added language default

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,12 +1,11 @@
-import en from "~/lang/en";
 import enUS from "~/lang/en-US";
 import ptBr from "~/lang/pt-BR";
 
 export default defineI18nConfig(nuxt => ({
   legacy: false,
-  // locale: "pt-BR",
+  locale: "en",
   messages: {
-    "en": en,
+    "en": enUS,
     "en-US": enUS,
     "pt-BR": ptBr
   }
