592bc854f74327f15859ee5ef77144ac398e3e40
Júlio César Lima Reis <juliolimareis@gmail.com>
2023-05-13 20:55:58 -0300

    Fix: Rename files

--- server/middleware/db.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient, } from "@prisma/client";
-
-declare global {
-  var db: PrismaClient;
-}
-
-export default defineEventHandler(() => {
-  if(!global.db) global.db = new PrismaClient();
-});
