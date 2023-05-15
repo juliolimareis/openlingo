42dc6b8e9ddad3278f28d601a78afde2cdf15a64
Júlio César Lima Reis <juliolimareis@gmail.com>
2023-05-14 23:49:41 -0300

    Fix: Added global types

--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,47 @@
-interface User {
-  id?: string;
-  name: string;
-  email: string;
-  passwd: string;
+import { H3Event, } from "h3";
+
+declare global {
+  interface User {
+    id?: string;
+    name: string;
+    email: string;
+    passwd: string;
+  }
+
+  interface Lesson {
+    id: number
+    lang: string
+    nativeLang: string
+    createUserId: number
+    isShare: number
+    createdAt: string
+    desc: string
+    title: string
+    interactions: Interaction[]
+  }
+
+  interface Interaction {
+    phrase: string
+    translation: string
+    urlImage: string
+    videoId: string
+    vInit: number
+    vEnd: number
+    about: string
+  }
+
+  interface DataToken {
+    id: number
+    email: string
+    createdAt: string
+    name: string
+    birthday: any
+    iat: number
+    exp: number
+  }
+
+  interface ApiEvent extends H3Event {
+    dataToken: DataToken
+  }
+
 }
