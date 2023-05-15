53ec4d929b5609f2f1de1fe912b432e135a05843
Júlio César Lima Reis <juliolimareis@gmail.com>
2023-05-14 23:48:03 -0300

    Fix: Added security in validation

--- server/utils/validations.ts
+++ server/utils/validations.ts
@@ -1,13 +1,58 @@
 import { validateOrReject, } from "class-validator";
+import { useJWT, } from "./jwt";
 
 export async function validateClass<T extends object>(validation: T) {
   return validateOrReject(validation);
 }
 
-export const basicUseSelectForToken = { id: true, email: true, createdAt: true, name: true, birthday: true };
+export const basicUseSelectForToken = { id: true, email: true, createdAt: true, name: true, birthday: true, passwd: false };
 
 export function setStatus(event: any, code: number, err: object){
   setResponseStatus(event, code);
 
   return err;
+}
+
+export function getDataTokenInHeader(event: any){
+  const headerAuth = event.node.req.headers.authorization as string;
+
+  if(headerAuth && headerAuth.includes("Bearer")){
+    const { decodeToken } = useJWT();
+    const token = headerAuth.split(" ")[1];
+    const dataToken = decodeToken(token);
+
+    if(dataToken){
+      return dataToken;
+    }
+  }
+
+  return false;
+}
+
+export function isPublicRouter(event: any){
+  const apiPublicRouters = useRuntimeConfig().apiPublicRouters as any;
+  const pathName = Object.keys(apiPublicRouters).find(k => k === event.path);
+
+  if (pathName){
+    return !!apiPublicRouters[pathName].includes(event?.node?.req?.method?.toLowerCase() ?? "");
+  }
+
+  return false;
+}
+
+export function handleSecurityRouter(event: any){
+  if(!isPublicRouter(event)){
+    const dataToken = getDataTokenInHeader(event);
+
+    if(dataToken){
+      event.dataToken = dataToken;
+
+      return true;
+    }
+
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Not authorized"
+    });
+  }
 }
