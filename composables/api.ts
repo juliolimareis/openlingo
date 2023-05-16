0ccc05d6ef1a7559cb166f90dae212a854e7a69b
Júlio César Lima Reis <juliolimareis@gmail.com>
2023-05-16 01:57:39 -0300

    Fix: Added token in request api

--- composables/api.ts
+++ composables/api.ts
@@ -1,3 +1,6 @@
 import axios from "axios";
 
-export const useApi = () => axios.create({ baseURL: "http://localhost:3000/api/v1" });
+export const useApi = (token?: string) => axios.create({
+  baseURL: "http://localhost:3000/api/v1",
+  headers: { Authorization: !!(process.client) ? `Bearer ${localStorage.token}` : "" }
+});
