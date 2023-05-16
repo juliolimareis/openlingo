e76cb277aeb0793afe6a747eb433de8900761fbf
Júlio César Lima Reis <juliolimareis@gmail.com>
2023-05-16 18:29:53 -0300

    Fix: Added config pinia-plugin-persistedstate

--- plugins/shared-state.client.ts
+++ plugins/shared-state.client.ts
@@ -1,6 +1,8 @@
 import { PiniaSharedState, } from "pinia-shared-state";
+import piniaPersistedstate from "pinia-plugin-persistedstate";
 
 // https://www.vuescript.com/sync-pinia-state-across-browser-tabs/
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.$pinia.use(PiniaSharedState({ enable: true }));
+  nuxtApp.$pinia.use(piniaPersistedstate);
 });
