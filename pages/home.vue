5e27fc8bc8780b49c924f2506b046368eb588f10
Júlio César Lima Reis <juliolimareis@gmail.com>
2023-05-16 02:03:06 -0300

    Feature: Creating lesson system

--- pages/home.vue
+++ pages/home.vue
@@ -132,14 +132,14 @@
                 <span class="sr-only">Open user menu</span>
                 <img class="h-8 w-8 rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                 <span class="hidden lg:flex lg:items-center">
-                  <span class="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">{{ context.$userData?.name }}</span>
+                  <span class="ml-4 text-sm font-semibold leading-6 text-gray-900" aria-hidden="true">{{ $state?.user?.name }}</span>
                   <ChevronDownIcon class="ml-2 h-5 w-5 text-gray-400" aria-hidden="true" />
                 </span>
               </MenuButton>
               <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                 <MenuItems class="absolute right-0 z-10 mt-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                   <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                    <a :href="item.href" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']">{{ item.name }}</a>
+                    <a @click="item.onClick()" :href="item.href" :class="[active ? 'bg-gray-50' : '', 'block px-3 py-1 text-sm leading-6 text-gray-900']">{{ item.name }}</a>
                   </MenuItem>
                 </MenuItems>
               </transition>
@@ -160,8 +160,6 @@
 
 <script setup>
 definePageMeta({ middleware: ["auth"] });
-const context = useNuxtApp();
-
 import { ref, } from "vue";
 import {
   Dialog,
@@ -187,12 +185,10 @@ import {
 } from "@heroicons/vue/24/outline";
 import { ChevronDownIcon, MagnifyingGlassIcon, } from "@heroicons/vue/20/solid";
 
-onMounted(() => {
-  context.$setUserData({ name: "Júlio" });
-});
+const { $state, logout } = useUserStore();
 
 const navigation = [
-  { name: "Lesson", href: "/home/lesson", icon: HomeIcon, current: true },
+  { name: "Lessons", href: "/home", icon: HomeIcon, current: true },
   { name: "Team", href: "/home/team", icon: UsersIcon, current: false },
   { name: "Projects", href: "#", icon: FolderIcon, current: false },
   { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
@@ -205,8 +201,8 @@ const teams = [
   { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
 ];
 const userNavigation = [
-  { name: "Your profile", href: "#" },
-  { name: "Sign out", href: "#" },
+  { name: "Your profile", href: "/profile" },
+  { name: "Sign out", href:"", onClick: () => logout() },
 ];
 
 const sidebarOpen = ref(false);
--- pages/lesson.vue
+++ pages/lesson.vue
@@ -1,50 +1,107 @@
 <template>
-  <Container>
-    <div class="flex">
-      <div @click="playAudio()" class="mr-6">
-        <SpeakerWaveIcon class="w-24 text-primary-500" />
-      </div>
-      <div @click="playAudio(0.5)">
-        <TruckIcon class="w-14 mt-5 text-primary-500" />
-      </div>
-    </div>
+  <div class="overflow-x-hidden">
+    <ProgressBar :progress="progress"/>
 
-    <div id="sound" class="invisible" aria-hidden></div>
-  </Container>
+    <Container>
+
+      <Speaker :interaction="interactions[interactionIndex]"/>
+
+      <div class="p-2">
+        <div class="flex justify-start mt-10">
+          <Word v-if="!wordsLine.length" class="invisible" word="本当" />
+          <Word
+            v-for="w in wordsLine"
+            :word="w.word"
+            @click="addOrRemoveWordLine(w)"
+          />
+        </div>
+        <hr />
+        <div class="flex justify-start">
+          <Word class="invisible" word="f" />
+        </div>
+        <hr />
+      </div>
+      <!-- Options -->
+
+      <div class="flex justify-center mt-10">
+        <Word
+          v-for="w in wordsOptions"
+          :word="w.word"
+          @click="addOrRemoveWordLine(w)"
+        />
+      </div>
+
+      <Alert :type="alertType">
+        <p v-if="isCorrect" class="text-sm">Correto.</p>
+
+        <div v-else>
+
+        </div>
+      </Alert>
+
+      <SizedBox v-if="hiddenButton" width="50" height="50"></SizedBox>
+
+      <div v-if="hiddenButton" class="p-5">
+        <Button class="w-[99%]">Conferir</Button>
+      </div>
+
+    </Container>
+  </div>
 </template>
 
 <script lang="ts" setup>
 definePageMeta({ middleware: ["auth"] });
 
-import { SpeakerWaveIcon, TruckIcon, } from "@heroicons/vue/24/solid";
+const { $state } = useUserStore();
+const interactionIndex = ref(0);
+const wordsOptions = ref<Word[]>([]);
+const wordsLine = ref<Word[]>([]);
+const progress = ref(0);
+const isCorrect = ref(false);
+const hiddenButton = ref(false);
+const alertType = ref<"success" | "error">("success");
+// const lines = ref([[]]);
 
-const player = ref();
-const videoId = ref("qANpuNm9Dkg");
+const interactions = ref($state.lessons[0].interactions);
 
-function onYouTubeIframeAPIReady() {
-  // https://developers.google.com/youtube/iframe_api_reference?hl=pt-br
-  player.value = new YT.Player("sound", {
-    height: "360",
-    width: "640",
-    videoId: "M7lc1UVf-VE",
-    suggestedQuality: "small"
-  });
+function addOrRemoveWordLine(word: Word){
+  if(wordsLine.value.some(w => w.id === word.id)){
+    wordsLine.value.splice(word.id, 1);
+    wordsOptions.value.push(word);
+  }else{
+    wordsOptions.value.splice(word.id, 1);
+    wordsLine.value.push(word);
+  }
 }
 
-function playAudio(backRate?: number){
-  player.value.setPlaybackRate(backRate ?? 1);
+function finishInteraction(){
+  hiddenButton.value = true;
+  // if()
+  nextInteraction();
+}
 
-  player.value.loadVideoById({
-    videoId: videoId.value,
-    startSeconds: 1,
-    endSeconds: 2.6,
-    playbackRate: 2
-  });
+function nextInteraction(){
+  if(interactionIndex.value < wordsLine.value.length){
+    interactionIndex.value++;
+    progress.value += 10;
+    handleWordsOptions();
+    hiddenButton.value = false;
+  }
+}
+
+function handleWordsOptions() {
+  wordsOptions.value = getActualInteraction().phrase[0].split(" ").map((word, i) => ({
+    id: i,
+    word
+  }));
+}
+
+function getActualInteraction(){
+  return interactions.value[interactionIndex.value];
 }
 
 onMounted(() => {
-  onYouTubeIframeAPIReady();
-
-  // fetch("/api/v1/user", { method: "POST" }).then(res => console.log(res));
+  handleWordsOptions();
 });
+
 </script>
